3c1870286f42b11bcfcbacafa6e86c23ad08bf46
Stephane-EQL <58989310+Stephane-EQL@users.noreply.github.com>
2021-06-18 11:39:05 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,18 +1,21 @@
 package com.example;
 
 /** Description */
-public class Greeter {
-  
+public class Greeter
+{
 
-  /** Description */
-  public Greeter() {
-    
+
+/** Description */
+  public Greeter ()
+  {
+
   }
-  
-  /** Description 
-  @return
-  */
-  public final String greet(final String someone) {    
-    return String.format("Hello stephane, %s!", someone);
+
+/** Description 
+@return
+*/
+  public final String greet (final String someone)
+  {
+    return String.format ("Hello stephane, %s!", someone);
   }
 }
