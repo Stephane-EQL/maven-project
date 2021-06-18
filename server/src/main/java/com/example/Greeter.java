c5504a62d8cf4d77d851815228ca2ecabf6d6737
Stephane-EQL <58989310+Stephane-EQL@users.noreply.github.com>
2021-06-18 12:17:00 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,13 +1,21 @@
 package com.example;
 
-
+/**
+* Retourne le level du zéro.
+*/
 public class Greeter {
 
- 
+/**
+  * Retourne le level du zéro.
+  */ 
   public Greeter() {
 
   }
 
+/**
+  * Retourne le level du zéro.
+  * @return Une instance de SDZLevel, qui correspond à niveau du membre sur SDZ.
+  */
   public final String greet(final String someone) {
     return String.format("Hello stephane, %s!", someone);
   }
