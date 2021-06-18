e2bb26d04b61c9061d5157834ed9f5c53a60952c
Stephane-EQL <58989310+Stephane-EQL@users.noreply.github.com>
2021-06-18 11:26:10 +0200

    Comment checkstyle

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -2,13 +2,14 @@ package com.example;
 
 
 public class Greeter {
-
+//comment public class Greeter
   
   public Greeter() {
-
+  //comment public Greeter
   }
 
   public final String greet(final String someone) {
+    //comment public final greet
     return String.format("Hello stephane, %s!", someone);
   }
 }
